69ce3e6d2a63a102b2c4df338d547b611b5fadf5
hongjinkwon <hjk0472@gmail.com>
2024-09-14 22:50:44 +0900

    :sparkles: Add Meeting and Todo events to Event List

--- src/main/java/com/fastcampus/calenderapplication/CalenderApplication.java
+++ src/main/java/com/fastcampus/calenderapplication/CalenderApplication.java
@@ -1,13 +1,30 @@
 package com.fastcampus.calenderapplication;
 
-import org.springframework.boot.SpringApplication;
+import com.fastcampus.calenderapplication.event.AbstractEvent;
+import com.fastcampus.calenderapplication.event.Event;
+import com.fastcampus.calenderapplication.event.eventType.Meeting;
+import com.fastcampus.calenderapplication.event.eventType.Todo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 @SpringBootApplication
 public class CalenderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalenderApplication.class, args);
-    }
+        List<AbstractEvent> list = new ArrayList<>();
+        HashSet<String> participants = new HashSet<>();
+        participants.add("glenHong");
 
+        Meeting meeting1 = new Meeting(1, "Meeting 1", ZonedDateTime.now(), ZonedDateTime.now().plusHours(1), participants, "MeetingRoomA", "Study");
+        list.add(meeting1);
+
+        Todo todo1 = new Todo(1, "Todo 1", ZonedDateTime.now(), ZonedDateTime.now().plusHours(1), "약국 다녀오기");
+        list.add(todo1);
+
+        list.forEach(Event::print);
+    }
 }
